68ea11c69fb8ea648a708710d5fc422b06c65829
Anupama_Halder <anupamahalder2022@gmail.com>
2023-12-23 19:11:39 +0530

    Added header to the site

---
+++ practice-project/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <About/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
---
+++ practice-project/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+const About = () => {
+    return (
+        <div className='about'>
+            <h1>Hello from About</h1>
+        </div>
+    );
+};
+
+export default About;
