f15f40a5b71860d34125364b85eeae90320615db
Anupama_Halder <anupamahalder2022@gmail.com>
2023-12-23 19:17:34 +0530

    Added router to route from different page to another page

--- practice-project/src/App.js
+++ practice-project/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <About/>
-      <Contact/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
