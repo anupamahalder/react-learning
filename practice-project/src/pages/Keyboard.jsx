f12a77976f49bf008e11f3e603ddc0818c5fc1a7
Anupama_Halder <anupamahalder2022@gmail.com>
2023-12-25 13:45:20 +0530

    Implement onKeyUp when we will will up our hand from a keyword

--- practice-project/src/pages/Keyboard.jsx
+++ practice-project/src/pages/Keyboard.jsx
@@ -4,10 +4,15 @@ const Keyboard = () => {
     const onkeydownText = (event) =>{
         console.log("key pressed ", event.key);
     }
+    // when we will up our hand from a keyword 
+    const onkeyupText = (event) =>{
+        console.log("Key up",event.key);
+    }
     return (
         <div>
             <h1>Keyboard Event</h1>
-            <input type="text" onKeyDown={onkeydownText} />
+            <input type="text" onKeyDown={onkeydownText} /> <br /> <br />
+            <input type="text" onKeyUp={onkeyupText} /> <br />
         </div>
     );
 };
