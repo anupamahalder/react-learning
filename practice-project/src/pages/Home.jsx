08a387c63a9d138bd125409fbde601c8e2d1187f
Anupama_Halder <anupamahalder2022@gmail.com>
2023-12-25 14:20:42 +0530

    Created an object for style and the put that object to style tag for inline style

--- practice-project/src/pages/Home.jsx
+++ practice-project/src/pages/Home.jsx
@@ -18,6 +18,7 @@ const Home = () => {
     const blurText = () =>{
         console.log("You have blur on input!");
     }
+    const textStyleForElement = {backgroundColor: '#620612', color:'white', textAlign: 'center', padding: '10px', textTransform:'uppercase'};
     return (
         <div>
             <Header/>
@@ -26,7 +27,7 @@ const Home = () => {
             <div>
                 <p onCopy={copyText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium distinctio vero, perspiciatis, impedit quia odio deleniti consectetur officia quibusdam illo incidunt fugit, officiis magni quis expedita error ipsa. Obcaecati ullam error eaque maxime voluptas eius hic voluptates eos illum exercitationem possimus, in assumenda a asperiores, quasi nisi. Cumque, doloribus ipsa.</p>
 
-                <h2 onCut={cutText}>This is clipboard page you have cut some text from here</h2>
+                <h2 style={textStyleForElement} onCut={cutText}>This is clipboard page you have cut some text from here</h2>
                 <label htmlFor="">Enter name: </label>
                 <input type="text" onPaste={pasteText}/> <br /><br />
                 <label htmlFor="">Enter email:</label>
